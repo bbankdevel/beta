773aea0ee611c149ddce62213a3ab3fbd3422146
root <root@iPhone>
2022-04-13 19:25:33 -0400

    on

--- src/app/elements/header/header.component.ts
+++ src/app/elements/header/header.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {NgbDropdownConfig} from '@ng-bootstrap/ng-bootstrap';
 import {UserWService} from '../../user-w.service';
+import { AuthService } from '../../auth.service';
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -13,12 +16,19 @@ export class HeaderComponent implements OnInit {
 	
 	constructor(
 public _uw:UserWService,
+   public router: Router,
+    private authService: AuthService,
+     private location: Location
 		) { }
 	
 	ngOnInit(): void {
 	}
 	
-	
+	 onlogoutUser():void{
+    this.authService.logoutUser();
+    this._uw.isLogged=false;
+    this.router.navigate(['/login']);
+  }
 	togglechatbar() {
 		this.toggleChat = !this.toggleChat;
 	}
