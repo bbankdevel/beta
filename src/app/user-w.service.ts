46ca0a76ed4cf6c9c6999116f44e6d602ad25c12
root <root@iPhone>
2022-04-13 16:53:30 -0400

    on

--- src/app/user-w.service.ts
+++ src/app/user-w.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class UserWService {
-	admin:boolean;
+	admin:boolean=false;
 	adminName:string;
 	allLoaded:boolean=false;
 	buttonDisabled:boolean=false;
