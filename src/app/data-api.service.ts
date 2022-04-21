84c58fee958ae3421aeb230a6111e00fc265c4a9
root <root@iPhone>
2022-04-21 00:15:36 -0400

    new accounts

--- src/app/data-api.service.ts
+++ src/app/data-api.service.ts
@@ -3,7 +3,6 @@ import { HttpClient, HttpHeaders }  from '@angular/common/http';
 import { Observable } from 'rxjs/internal/Observable';
 import { map } from 'rxjs/operators';
 import { AuthService } from './auth.service';
-import { UsercardInterface } from './models/usercard-interface';
 import { EmailmInterface } from './models/emailm-interface';
 import { UserWService } from "./user-w.service";
 import { InfoInterface } from './models/info-interface';
@@ -13,11 +12,10 @@ import { AccountInterface } from './models/account-interface';
   providedIn: 'root'
 })
 export class DataApiService {	
-	usercard: Observable<any>;
+
 	emailm: Observable<any>;
 	usercards: Observable<any>;
 	accounts: Observable<any>;
-	cards: Observable<any>;
 	info: Observable<any>;
   constructor(
   	public _uw:UserWService,
@@ -33,10 +31,6 @@ export class DataApiService {
 		return (this.info);
 	}
 
-	getAllUsercardsReturn(){
-		const url_api = 'https://db.bbevolutionbank.com:3025/api/usercard?filter[where][status]=activated';
-		return (this.usercards = this.http.get(url_api));
-	}
 	getActiveAccountsReturn(){
 		const url_api = 'https://db.bbevolutionbank.com:3025/api/account?filter[where][status]=active';
 		return (this.accounts = this.http.get(url_api));
@@ -56,12 +50,7 @@ export class DataApiService {
 		.post(url_api, emailm)
 		.pipe(map(data => data));
 	}
-	saveUsercard(usercard :UsercardInterface){
-		const url_api='https://db.bbevolutionbank.com:3025/api/card';
-		return this.http
-		.post<UsercardInterface>(url_api, usercard)
-		.pipe(map(data => data));
-	}
+
 	saveAccount(account :AccountInterface){
 		const url_api='https://db.bbevolutionbank.com:3025/api/account';
 		return this.http
@@ -69,18 +58,18 @@ export class DataApiService {
 		.pipe(map(data => data));
 	}
 			
-	updateUsercard(usercard :UsercardInterface, id: string){
-		const url_api=`https://db.bbevolutionbank.com:3025/api/card/${id}`;
+	updateAccount(account :AccountInterface, id: string){
+		const url_api=`https://db.bbevolutionbank.com:3025/api/account/${id}`;
 		return this.http
-		.put<UsercardInterface>(url_api, usercard)
+		.put<AccountInterface>(url_api, account)
 		.pipe(map(data => data));
 	}
 		
-	getUsercardByUserd2(userd: string){
+	getAccountByUserd2(userd: string){
 		let indice = userd;
-		const url_api =  "https://db.bbevolutionbank.com:3025/api/card?filter[where][userId]=p"+indice;
-		this.usercard = this.http.get(url_api);
-		return (this.usercard);
+		const url_api =  "https://db.bbevolutionbank.com:3025/api/account?filter[where][userId]=p"+indice;
+		this.account = this.http.get(url_api);
+		return (this.account);
 	}
 	
 }
