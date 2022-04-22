13f69bb6be0fe8eefd54446ec735ef49a23535db
bbankdevel <102521839+bbankdevel@users.noreply.github.com>
2022-04-22 08:26:32 -0400

    acti

--- src/app/admin/activateaccount/activateaccount.component.ts
+++ src/app/admin/activateaccount/activateaccount.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DataApiService } from '../../data-api.service';
+import { UserWService } from "../../user-w.service";
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-activateaccount',
   templateUrl: './activateaccount.component.html',
   styleUrls: ['./activateaccount.component.css']
 })
 export class ActivateaccountComponent implements OnInit {
-
-  constructor() { }
-
+  ngFormLogin: FormGroup;
+  submitted = false;
+  constructor(
+    public _uw:UserWService,
+    public router: Router,
+    public dataApi:DataApiService
+  ) { }
+  public isError = false;
+  public isLogged =false;
+ public user : UserInterface ={
+    name:"",
+    email:"",
+    password:"",
+    usertype:""
+  };
+  message = "";  
+  get fval() {
+    return this.ngFormLogin.controls;
+    }
+  onIsError(): void {
+       
+      this.isError = true;
+      setTimeout(() => {
+      this.isError = true;
+        //this.isError = false;
+      }, 4000);
+    }
   ngOnInit(): void {
+    this.ngFormActivateAccount = this.formBuilder.group({
+      numberAccount: ['', [Validators.required]]
+      });
   }
     
   
