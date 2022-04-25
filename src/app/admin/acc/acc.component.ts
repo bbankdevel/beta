cdb2b010a6ce9ec0c0968094ec1191c48d379196
bbankdevel <102521839+bbankdevel@users.noreply.github.com>
2022-04-25 05:20:18 -0500

    all now

---
+++ src/app/admin/acc/acc.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-acc',
+  templateUrl: './acc.component.html',
+  styleUrls: ['./acc.component.css']
+})
+export class AccComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+    
+  
+ //  checkUncheckAll(event) {
+ //     var checkboxes = document.getElementsByTagName('input');
+ //     if (event.target.checked) {
+ //         for (var i = 0; i < checkboxes.length; i++) {
+ //             if (checkboxes[i].type == 'checkbox') {
+ //                 checkboxes[i].checked = true;
+ //             }
+ //         }
+ //     } else {
+ //         for (var i = 0; i < checkboxes.length; i++) {
+ //             // console.log(i)
+ //             if (checkboxes[i].type == 'checkbox') {
+ //                 checkboxes[i].checked = false;
+ //             }
+ //         }
+ //     }
+ // }
+  
+  
+  
+
+}
