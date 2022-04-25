d7d757a8d020d6d6192b7758aaddbcb25e030ee9
bbankdevel <102521839+bbankdevel@users.noreply.github.com>
2022-04-25 06:12:56 -0500

    all now

--- src/app/admin/settings/settings.component.ts
+++ src/app/admin/settings/settings.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DataApiService } from '../../data-api.service';
+import { UserWService } from "../../user-w.service";
+import { InfoInterface } from '../../models/info-interface'; 
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
+import { isError } from "util";
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent implements OnInit {
+  ngFormActivateSettingsUpdate: FormGroup;
+  submitted = false;
+  constructor(
+    public _uw:UserWService,
+    public router: Router,
+    private formBuilder: FormBuilder,
+    public dataApi:DataApiService
 
-  constructor() { }
-
+  ) { }
+  public info : InfoInterface;
+  public isError = false;
+  public isLogged =false;
+  get fval() {
+    return this.ngFormActivateSettingsUpdate.controls;
+    }
+    onIsError(): void {
+       
+      this.isError = true;
+      setTimeout(() => {
+      this.isError = true;
+        //this.isError = false;
+      }, 4000);
+    }
   ngOnInit(): void {
+    
+    this.ngFormActivateSettingsUpdate = this.formBuilder.group({
+      adminEmail: ['', [Validators.required]]
+      });
+  }
   }
     
   
